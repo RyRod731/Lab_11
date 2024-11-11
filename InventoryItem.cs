7d80b43d251cf329b7da71ab6d7122dae398a0f2
Ryan Rodriguez <144503255+RyRod731@users.noreply.github.com>
2024-11-10 23:37:26 -0500

    Add files via upload

---
+++ InventoryItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public int BinarySearch (item);
+
+        Add BinarySearchByID();
+
+        sort(Inventory by ID into BinarySearch);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ InventoryManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         Add LinearSearchByName();
+
+         string (itemName)
+
+         return InventoryItem();
+
+         if (item is found);
+
+         else (item is not found = null);
+    }
+
+    Add QuickSortByValue();
+
+    sort(InventoryItems by Value);
+
+}
